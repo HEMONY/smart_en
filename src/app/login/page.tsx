79b3f4a2d405bc2648222379b95f25a01e859ba1
HEMONY <71093276+HEMONY@users.noreply.github.com>
2025-07-07 23:16:52 +0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -31,18 +31,10 @@ export default function LoginPage() {
               <p className="text-sm text-gray-400 mb-3">
                 قم بتسجيل الدخول باستخدام حساب تيليجرام الخاص بك. سيتم إرسال رمز تحقق إلى بوت تيليجرام الخاص بنا.
               </p>
-              <div id="telegram-login" className="flex justify-center mt-2">
-                <script
-                  async
-                  src="https://telegram.org/js/telegram-widget.js?7"
-                  data-telegram-login="SMARtcoinNbot"  // <-- غيّر إلى معرف البوت الخاص بك
-                  data-size="large"
-                  data-userpic="true"
-                  data-radius="10"
-                  data-auth-url="https://smart-en.vercel.app/api/auth/telegram/route.js"  // تأكد من المسار الصحيح
-                  data-request-access="write"
-                ></script>
-              </div>
+              <Link href="/api/auth/telegram" className="primary-button w-full">
+                <FaTelegramPlane size={20} />
+                <span>تسجيل الدخول عبر تيليجرام</span>
+              </Link>
 
             </div>
             
