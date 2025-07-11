6444f67b1b32eb1d0c1142f53a66382041dc3165
HEMONY <71093276+HEMONY@users.noreply.github.com>
2025-07-11 10:58:07 +0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -64,7 +64,7 @@ export default function LoginPage() {
                 <script
                   async
                   src="https://telegram.org/js/telegram-widget.js?7"
-                  data-telegram-login="SmartCoinBot" // <-- غيّر هذا لاسم البوت بدون @
+                  data-telegram-login="ASMARTCOINBOT" // <-- غيّر هذا لاسم البوت بدون @
                   data-size="large"
                   data-userpic="true"
                   data-lang="ar"
