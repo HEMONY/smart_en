cb3f2eca3c11d63178fe3fdb843a51b66bd245c3
HEMONY <71093276+HEMONY@users.noreply.github.com>
2025-07-11 11:42:03 +0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -63,7 +63,7 @@ export default function LoginPage() {
     const script = document.createElement('script');
     script.src = 'https://telegram.org/js/telegram-widget.js?7';
     script.async = true;
-    script.setAttribute('data-telegram-login', 'ASMARTCOINBOT'); // <-- اسم البوت بدون @
+    script.setAttribute('data-telegram-login', 'Tesmiapbot'); // <-- اسم البوت بدون @
     script.setAttribute('data-size', 'large');
     script.setAttribute('data-userpic', 'true');
     script.setAttribute('data-lang', 'ar');
