43d71ebe1a47644a1ef25f3ed62a18dd0fce52ac
HEMONY <71093276+HEMONY@users.noreply.github.com>
2025-07-15 00:18:15 +0300

    Update server.js

--- src/lib/supabase/server.js
+++ src/lib/supabase/server.js
@@ -5,8 +5,8 @@ export function createClient() {
   const cookieStore = cookies();
   
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://aqunpkwwvslnmuqvotyl.supabase.co',
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxdW5wa3d3dnNsbm11cXZvdHlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0NDAyODQsImV4cCI6MjA2MTAxNjI4NH0.FgbKG2DyKL7Ob6fIfwaC43_jAphP7YG2D61IgIIVHpo',
+    process.env.NEXT_PUBLIC_SUPABASE_URL ||'https://xsxbeihsavosrxjyzmga.supabase.co',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzeGJlaWhzYXZvc3J4anl6bWdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1NjI2ODEsImV4cCI6MjA2NzEzODY4MX0.79iTU8QrexMFSrg_CesL_vOSwQ0TWxq3iN8TsVDWE-o',
     {
       cookies: {
         get: (name) => cookieStore.get(name)?.value,
