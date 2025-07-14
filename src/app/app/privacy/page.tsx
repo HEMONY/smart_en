3f937a64db65ad77830120ef4ae8cabf67059f4c
HEMONY <71093276+HEMONY@users.noreply.github.com>
2025-07-14 23:16:41 +0300

    Delete src/app/app directory

--- src/app/app/privacy/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function PrivacyPage() {
-  return (
-    <div className="p-6 text-white">
-      <h1 className="text-3xl font-bold mb-4">سياسة الخصوصية</h1>
-      <p>هذه صفحة سياسة الخصوصية... يمكنك تعديل النص كما يناسبك.</p>
-    </div>
-  );
-}
