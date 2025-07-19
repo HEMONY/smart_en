b619aecbbf441f2541f82823c98ca1699c45675e
HEMONY <71093276+HEMONY@users.noreply.github.com>
2025-07-19 14:14:31 +0300

    Create page.tsx

---
+++ src/app/term/page.tsx
@@ -0,0 +1,12 @@
+export default function PrivacyPage() {
+  return (
+    <div className="p-6 text-white">
+      <h1 className="text-3xl font-bold mb-4">📜 سياسة الخصوصية – SmartCoin</h1>
+      <p>
+
+
+
+      </p>
+    </div>
+  );
+}
